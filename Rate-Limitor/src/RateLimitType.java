8f70df7b5d549845a0e29a7921a669b3b7e2f166
laggerk24 <laggerk24@gmail.com>
2025-11-02 21:21:14 +0530

    Rate Limitor Change set 2

---
+++ Rate-Limitor/src/RateLimitType.java
@@ -0,0 +1,8 @@
+
+public enum RateLimitType {
+    TOKEN_BUCKET,
+    LEAKY_BUCKET,
+    FIXED_WINDOW,
+    SLIDING_WINDOW_LOG,
+    SLIDING_WINDOW_COUNTER
+}
---
+++ Rate-Limitor/src/UserTier.java
@@ -0,0 +1,5 @@
+
+public enum UserTier {
+    FREE,
+    PREMIUM
+}
